c2a6bc203b57e232e685b92977018c83921cad78
Jibrian Goodwin <jjgoodwin346@gmail.com>
2026-02-22 02:31:15 -0500

    added a user page that shows their details and what items they own

--- src/components/ui/ItemsData.jsx
+++ src/components/ui/ItemsData.jsx
@@ -5,24 +5,26 @@ import { faShoppingBag } from "@fortawesome/free-solid-svg-icons";
 
 const ItemsData = ({ item }) => {
   return (
-    <Link to={`/item/${item.itemId}`} className="item">
-      <figure className="item__img__wrapper">
-        <img src={item.imageLink} alt="" className="item__img" />
-      </figure>
-      <div className="item__details">
-        <span className="item__details__name">{item.title}</span>
-        <span className="item__details__price">{item.price} ETH</span>
-        <span className="item__details__last-sale">
-          Last sale: {item.lastSale} ETH
-        </span>
-      </div>
-      <div className="item__see-more">
-        <button className="item__see-more__button">See More</button>
-        <div className="item__see-more__icon">
-          <FontAwesomeIcon icon={faShoppingBag} />
+    <div className="item-column" style={{ width: "100%" }}>
+      <Link to={`/item/${item.itemId}`} className="item">
+        <figure className="item__img__wrapper">
+          <img src={item.imageLink} alt="" className="item__img" />
+        </figure>
+        <div className="item__details">
+          <span className="item__details__name">{item.title}</span>
+          <span className="item__details__price">{item.price} ETH</span>
+          <span className="item__details__last-sale">
+            Last sale: {item.lastSale} ETH
+          </span>
         </div>
-      </div>
-    </Link>
+        <div className="item__see-more">
+          <button className="item__see-more__button">See More</button>
+          <div className="item__see-more__icon">
+            <FontAwesomeIcon icon={faShoppingBag} />
+          </div>
+        </div>
+      </Link>
+    </div>
   );
 };
 
